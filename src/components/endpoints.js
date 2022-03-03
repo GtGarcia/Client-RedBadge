fe43a3705f34a9ec6dfc966e809c156473902a54
Guillermo Torres <gt.garcia0515@gmail.com>
2022-03-03 10:53:30 -0500

    first commit

---
+++ src/components/endpoints.js
@@ -0,0 +1,20 @@
+export const EndPoints = {
+    user: {
+        register: '/user/register',
+        login: '/user/login'
+    },
+    shoe: {
+        create: '/shoe/create',
+        update: '/shoe/',
+        delete: '/shoe/',
+        seeAll: '/shoe/',
+        seeMy: '/shoe/mine'
+    },
+    clothes: {
+        create: '/clothes/create',
+        update: '/clothes/',
+        delete: '/clothes/',
+        seeAll: '/clothes/',
+        seeMy: '/clothes/mine'
+    }
+}
